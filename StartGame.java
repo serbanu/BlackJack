29d57ed52c6c00e7a5107a616da88e2f64a2e289
serbanu <serban.stefanita@gmail.com>
2013-07-06 02:24:32 +0300

    A short and incomplete description of the idea

---
+++ StartGame.java
@@ -0,0 +1,29 @@
+
+public class StartGame {
+//Start game !
+//Spread the cards
+//	Player:
+//Verify if he has any Aces ---> if Yes, call chooseAce()
+//							---> else, verify if sumCard() returns a number greater	
+//	than 16 ----> if Yes, call stand()
+//			----> else, call hitMe()			--> that's a loop -- while(sumCard() < 16)
+//
+//	while(getSumCumulated() < 16) {
+//		hitMe();
+//		boolean checkSumCumulated();    < 16 ---if(true) {
+//			hitMe();
+//		} else {	stand(); }
+//	}
+//
+//	Table:	
+//Has one card first; Also, verifies if it's an 'Ace'
+//	if(it is)
+//		turn it into an 11 and stand()
+//	else
+//		hitMe();
+//		if(sumCumulated > 17)
+//			stand();
+//		else
+//			hitMe();
+//
+}
